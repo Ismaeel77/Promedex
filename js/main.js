a69321012bf65751f342e22432a76a4effcc8fdb
Ismaeel Abuzahra <abozahra.ismaeel@yahoo.com>
2024-03-21 15:41:13 +0200

    add faq section

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var swiper = new Swiper(".mySwiper", {
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
+    clickable: true,
   },
   mousewheel: true,
   breakpoints: {
@@ -24,7 +25,7 @@ var swiper = new Swiper(".mySwiper", {
   },
 });
 
-var swiper = new Swiper(".news-slider", {
+var newsSwiper = new Swiper(".news-slider", {
   slidesPerView: 3,
   spaceBetween: 10,
   grabCursor: true,
@@ -32,6 +33,7 @@ var swiper = new Swiper(".news-slider", {
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
+    clickable: true,
   },
   mousewheel: true,
   breakpoints: {
@@ -47,31 +49,44 @@ var swiper = new Swiper(".news-slider", {
   },
 });
 
-var swiper = new Swiper(".Slider", {
+var sliderSwiper = new Swiper(".Slider", {
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
 
+var partnerSwiper = new Swiper(".partners-slider", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  grabCursor: true,
+  mousewheel: true,
+  fade: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+
 // Control About/Investors Section
-let tabToggler = document.querySelectorAll("#about-us .about-investor")
+let tabToggler = document.querySelectorAll("#about-us .about-investor");
 
 tabToggler.forEach((tab) => {
-
-  tab.addEventListener("click", handleActiveNews)
+  tab.addEventListener("click", handleActiveNews);
   tab.addEventListener("click", (e) => {
     let sectionHead = document.querySelector("#about-us .about-investor-head");
     sectionHead.textContent = e.target.textContent;
-  })
-})
+  });
+});
 
-// Handle Active State 
-function handleActiveNews () {
+// Handle Active State
+function handleActiveNews() {
   tabToggler.forEach((tab) => {
-    tab.classList.remove("active")
+    tab.classList.remove("active");
     this.classList.add("active");
-  })
+  });
 }
 
 // Control News Section
@@ -83,34 +98,68 @@ let mainText = document.querySelector("#news .main-text");
 
 let cards = document.querySelectorAll("#news .news-card");
 
-
 cards.forEach((card) => {
-  card.addEventListener("click", handleActiveCards)
+  card.addEventListener("click", handleActiveCards);
   card.addEventListener("click", () => {
     let cardImg = card.querySelector("img");
     mainImg.src = cardImg.src;
     let cardTitle = card.querySelector("h6").textContent;
-    mainTitle.textContent = cardTitle
+    mainTitle.textContent = cardTitle;
     let cardText = card.querySelector("p").textContent;
     mainText.textContent = cardText;
   });
 });
 
-
-// Handle Active State 
-function handleActiveCards () {
+// Handle Active State
+function handleActiveCards() {
   cards.forEach((card) => {
-    card.classList.remove("active")
+    card.classList.remove("active");
     this.classList.add("active");
-  })
+  });
 }
 
-let docTitle = document.querySelector("title")
-let navLinks = document.querySelectorAll(".navbar .navbar-nav .nav-item .nav-link")
+let docTitle = document.querySelector("title");
+let navLinks = document.querySelectorAll(
+  ".navbar .navbar-nav .nav-item .nav-link"
+);
 navLinks.forEach((nav) => {
-  if(nav.classList.contains("active") === true){
-    docTitle.textContent = `${docTitle.textContent} | ${nav.textContent}`
+  if (nav.classList.contains("active") === true) {
+    docTitle.textContent = `${docTitle.textContent} | ${nav.textContent}`;
   } else {
     docTitle.textContent = docTitle.textContent;
   }
-})
+});
+
+// Show More Questions
+const moreBtn = document.querySelector("#faq .questions-container .more-btn");
+const questionsSection = document.querySelector("#faq .questions-container");
+
+moreBtn.addEventListener("click", () => {
+  // Create Question Container Div
+  let queDiv = document.createElement("div");
+  queDiv.setAttribute("class", "question mb-5");
+  // Create Question P
+  let quesP = document.createElement("p");
+  quesP.setAttribute("class", "ques");
+  // Create Question Number Span
+  let quesSpan = document.createElement("span");
+  quesSpan.setAttribute("class", "ques-num");
+  quesP.appendChild(quesSpan);
+  quesP.textContent = "what's your favourite product and why?";
+  // Create Answer Span
+  let spanAnswer = document.createElement("span");
+  spanAnswer.textContent =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo nisi harum illum animi quaerat dicta sequi, sint laborum dolornisi harum illum animi quaerat dicta sequi.";
+  queDiv.appendChild(quesP);
+  queDiv.appendChild(spanAnswer);
+  questionsSection.removeChild(moreBtn);
+  questionsSection.appendChild(queDiv)
+  questionsSection.appendChild(moreBtn);
+});
+
+// FAQ Section Numbering Questions
+let questions = document.querySelectorAll("#faq .question");
+for (let i = 0; i < questions.length; i++) {
+  let quesNum = document.querySelectorAll("span.ques-num");
+  quesNum[i].textContent = `Q${i}: `;
+}
