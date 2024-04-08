3e131ce866a4b9b94142a944ae9031a92f8cdd7b
Ismaeel Abuzahra <abozahra.ismaeel@yahoo.com>
2024-04-08 23:31:02 +0200

    edits

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-let docTitle = document.querySelector("title");
-let navLinks = document.querySelectorAll(
-  ".navbar .navbar-nav .nav-item a.nav-link"
-);
+function docTitle() {
+  let docTitle = document.querySelector("title");
+let navLinks = document.querySelectorAll(".navbar .navbar-nav .nav-item a.nav-link");
 navLinks.forEach((nav) => {
   if (nav.classList.contains("active") === true) {
     docTitle.textContent = `${docTitle.textContent} | ${nav.textContent}`;
@@ -9,122 +8,140 @@ navLinks.forEach((nav) => {
     docTitle.textContent = docTitle.textContent;
   }
 });
+}
 
-let searchIcon = document.getElementById("search-icon");
-searchIcon.addEventListener("click", () => {
-  let overlay = document.querySelector("div.search-bar-overlay");
-  overlay.style.display = "block";
+function searchIcon() {
+  let searchIcon = document.getElementById("search-icon");
+  searchIcon.addEventListener("click", () => {
+    let overlay = document.querySelector("div.search-bar-overlay");
+    overlay.style.display = "block";
 
-  let closeBtn = document.querySelector("i.close-btn");
-  // Add Event Click On Span To Close
+    let closeBtn = document.querySelector("i.close-btn");
+    // Add Event Click On Span To Close
 
-  closeBtn.onclick = function () {
-    overlay.style.display = "none";
-  };
-});
-
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  grabCursor: true,
-  fade: true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  mousewheel: true,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    767: {
-      slidesPerView: 3,
-    },
-    991: {
-      slidesPerView: 4,
-    },
-  },
-});
-
-var newsSwiper = new Swiper(".news-slider", {
-  slidesPerView: 3,
-  spaceBetween: 10,
-  grabCursor: true,
-  fade: true,
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
-  mousewheel: true,
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    991: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-var sliderSwiper = new Swiper(".Slider", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
-
-// Control About/Investors Section
-let tabToggler = document.querySelectorAll("#about-us .about-investor");
-
-tabToggler.forEach((tab) => {
-  tab.addEventListener("click", handleActiveNews);
-  tab.addEventListener("click", (e) => {
-    let sectionHead = document.querySelector("#about-us .about-investor-head");
-    sectionHead.textContent = e.target.textContent;
-  });
-});
-
-// Handle Active State
-function handleActiveNews() {
-  tabToggler.forEach((tab) => {
-    tab.classList.remove("active");
-    this.classList.add("active");
+    closeBtn.onclick = function () {
+      overlay.style.display = "none";
+    };
   });
 }
 
-// Control News Section
-let mainImg = document.querySelector("#news .img-holder img");
+docTitle()
+searchIcon()
+newsHandler()
+investorAbout()
 
-let mainTitle = document.querySelector("#news h2.news-headline");
+// selecting required element
+const ulPagination = document.querySelector("ul.pagination");
+let totalPages = 5;
+let page = 1;
+//calling function with passing parameters and adding inside element which is ul tag
+ulPagination.innerHTML = createPagination(totalPages, page);
+function createPagination(totalPages, page){
+  let liTag = '';
+  let active;
+  let beforePage = page - 1;
+  let afterPage = page + 1;
+  if(page > 1){ //show the next button if the page value is greater than 1
+    liTag += `<li class="page-item" onclick="createPagination(totalPages, ${page - 1})"><span class="page-link"><i class="fas fa-angle-left"></i> Prev</span></li>`;
+  }
 
-let mainText = document.querySelector("#news .main-text");
+  if(page > 2){ //if page value is less than 2 then add 1 after the previous button
+    liTag += `<li class="page-item" onclick="createPagination(totalPages, 1)"><span class="page-link">1</span></li>`;
+    if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
+      liTag += `<li class="page-item"><span class="page-link">...</span></li>`;
+    }
+  }
 
-let cards = document.querySelectorAll("#news .main-card");
+  // how many pages or li show before the current li
+  if (page == totalPages) {
+    beforePage = beforePage - 2;
+  } else if (page == totalPages - 1) {
+    beforePage = beforePage - 1;
+  }
+  // how many pages or li show after the current li
+  if (page == 1) {
+    afterPage = afterPage + 2;
+  } else if (page == 2) {
+    afterPage  = afterPage + 1;
+  }
 
-cards.forEach((card) => {
-  card.addEventListener("click", handleActiveCards);
-  card.addEventListener("click", () => {
-    let cardImg = card.querySelector("img");
-    mainImg.src = cardImg.src;
-    let cardTitle = card.querySelector("h6").textContent;
-    mainTitle.textContent = cardTitle;
-    let cardText = card.querySelector("p").textContent;
-    mainText.textContent = cardText;
+  for (var plength = beforePage; plength <= afterPage; plength++) {
+    if (plength > totalPages) { //if plength is greater than totalPage length then continue
+      continue;
+    }
+    if (plength == 0) { //if plength is 0 than add +1 in plength value
+      plength = plength + 1;
+    }
+    if(page == plength){ //if page is equal to plength than assign active string in the active variable
+      active = "active";
+    }else{ //else leave empty to the active variable
+      active = "";
+    }
+    liTag += `<li class="page-item ${active}" onclick="createPagination(totalPages, ${plength})"><span class="page-link">${plength}</span></li>`;
+  }
+
+  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
+    if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+      liTag += `<li class="page-item"><span class="page-link">...</span></li>`;
+    }
+    liTag += `<li class="page-item last" onclick="createPagination(totalPages, ${totalPages})"><span class="page-link">${totalPages}</span></li>`;
+  }
+
+  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+    liTag += `<li class="page-item" onclick="createPagination(totalPages, ${page + 1})"><span class="page-link">Next <i class="fas fa-angle-right"></i></span></li>`;
+  }
+  ulPagination.innerHTML = liTag; //add li tag inside ul tag
+  return liTag; //reurn the li tag
+}
+
+function investorAbout() {
+  // Control About/Investors Section
+  let tabToggler = document.querySelectorAll("#about-us .about-investor");
+
+  tabToggler.forEach((tab) => {
+    tab.addEventListener("click", handleActiveNews);
+    tab.addEventListener("click", (e) => {
+      let sectionHead = document.querySelector("#about-us .about-investor-head");
+      sectionHead.textContent = e.target.textContent;
+    });
   });
-});
 
-// Handle Active State
-function handleActiveCards() {
+  // Handle Active State
+  function handleActiveNews() {
+    tabToggler.forEach((tab) => {
+      tab.classList.remove("active");
+      this.classList.add("active");
+    });
+  }
+}
+
+function newsHandler() {
+  // Control News Section
+  let mainImg = document.querySelector("#news .img-holder img");
+
+  let mainTitle = document.querySelector("#news h2.news-headline");
+
+  let mainText = document.querySelector("#news .main-text");
+
+  let cards = document.querySelectorAll("#news .main-card");
+
   cards.forEach((card) => {
-    card.classList.remove("active");
-    this.classList.add("active");
+    card.addEventListener("click", handleActiveCards);
+    card.addEventListener("click", () => {
+      let cardImg = card.querySelector("img");
+      mainImg.src = cardImg.src;
+      let cardTitle = card.querySelector("h6").textContent;
+      mainTitle.textContent = cardTitle;
+      let cardText = card.querySelector("p").textContent;
+      mainText.textContent = cardText;
+    });
   });
+
+  // Handle Active State
+  function handleActiveCards() {
+    cards.forEach((card) => {
+      card.classList.remove("active");
+      this.classList.add("active");
+    });
+  }
 }
