2a5c08fbc83f0770b4a67ff4f5bd4a87749e35e6
Ismaeel Abuzahra <abozahra.ismaeel@yahoo.com>
2024-04-28 09:48:13 +0300

    edit-responsive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+docTitle()
+investorAbout()
+newsHandler()
+newsSwiper()
+productSwiper()
+paginate()
+
 function docTitle() {
   let docTitle = document.querySelector("title");
   let navLinks = document.querySelectorAll(".navbar .navbar-nav .nav-item a.nav-link");
@@ -10,97 +17,70 @@ function docTitle() {
   });
 }
 
-function searchIcon() {
-  let searchIcon = document.getElementById("search-icon");
-  searchIcon.addEventListener("click", () => {
-    let overlay = document.querySelector("div.search-bar-overlay");
-    overlay.style.display = "block";
-    let closeBtn = document.querySelector("i.close-btn");
-    
-    overlay.onclick = (e) => {
-      if (e.target.getAttribute("class") === "search-bar-overlay") {
-        overlay.style.display = "none";
+function paginate() {
+  // selecting required element
+  const ulPagination = document.querySelector("ul.pagination");
+  let totalPages = 5;
+  let page = 1;
+  function createPagination(totalPages, page){
+    let liTag = '';
+    let active;
+    let beforePage = page - 1;
+    let afterPage = page + 1;
+    if(page > 1){ //show the next button if the page value is greater than 1
+      liTag += `<li class="page-item" onclick="createPagination(totalPages, ${page - 1})"><a class="page-link" href="#"><i class="fas fa-angle-left"></i> Prev</a></li>`;
+    }
+
+    if(page > 2){ //if page value is less than 2 then add 1 after the previous button
+      liTag += `<li class="page-item" onclick="createPagination(totalPages, 1)"><a class="page-link" href="#">1</a></li>`;
+      if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
+        liTag += `<li class="page-item"><a class="page-link">...</a></li>`;
       }
     }
 
-    // Add Event Click On Span To Close
-    closeBtn.onclick = () => {
-      overlay.style.display = "none";
-    };
-    
-    
-  });
-}
-
-productSwiper()
-newsSwiper()
-docTitle()
-searchIcon()
-newsHandler()
-investorAbout()
-
-// selecting required element
-const ulPagination = document.querySelector("ul.pagination");
-let totalPages = 5;
-let page = 1;
-//calling function with passing parameters and adding inside element which is ul tag
-ulPagination.innerHTML = createPagination(totalPages, page);
-function createPagination(totalPages, page){
-  let liTag = '';
-  let active;
-  let beforePage = page - 1;
-  let afterPage = page + 1;
-  if(page > 1){ //show the next button if the page value is greater than 1
-    liTag += `<li class="page-item" onclick="createPagination(totalPages, ${page - 1})"><a class="page-link" href="#"><i class="fas fa-angle-left"></i> Prev</a></li>`;
-  }
-
-  if(page > 2){ //if page value is less than 2 then add 1 after the previous button
-    liTag += `<li class="page-item" onclick="createPagination(totalPages, 1)"><a class="page-link" href="#">1</a></li>`;
-    if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
-      liTag += `<li class="page-item"><a class="page-link">...</a></li>`;
+    // how many pages or li show before the current li
+    if (page == totalPages) {
+      beforePage = beforePage - 2;
+    } else if (page == totalPages - 1) {
+      beforePage = beforePage - 1;
     }
+    // how many pages or li show after the current li
+    if (page == 1) {
+      afterPage = afterPage + 2;
+    } else if (page == 2) {
+      afterPage  = afterPage + 1;
+    }
+
+    for (var plength = beforePage; plength <= afterPage; plength++) {
+      if (plength > totalPages) { //if plength is greater than totalPage length then continue
+        continue;
+      }
+      if (plength == 0) { //if plength is 0 than add +1 in plength value
+        plength = plength + 1;
+      }
+      if(page == plength){ //if page is equal to plength than assign active string in the active variable
+        active = "active";
+      }else{ //else leave empty to the active variable
+        active = "";
+      }
+      liTag += `<li class="page-item ${active}" onclick="createPagination(totalPages, ${plength})"><a class="page-link" href="#">${plength}</a></li>`;
+    }
+
+    if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
+      if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+        liTag += `<li class="page-item"><a class="page-link" href="#">...</a></li>`;
+      }
+      liTag += `<li class="page-item last" onclick="createPagination(totalPages, ${totalPages})"><a class="page-link" href="#">${totalPages}</a></li>`;
+    }
+
+    if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+      liTag += `<li class="page-item" onclick="createPagination(totalPages, ${page + 1})"><a class="page-link" href="#">Next <i class="fas fa-angle-right"></i></a></li>`;
+    }
+    ulPagination.innerHTML = liTag; //add li tag inside ul tag
+    return liTag; //reurn the li tag
   }
 
-  // how many pages or li show before the current li
-  if (page == totalPages) {
-    beforePage = beforePage - 2;
-  } else if (page == totalPages - 1) {
-    beforePage = beforePage - 1;
-  }
-  // how many pages or li show after the current li
-  if (page == 1) {
-    afterPage = afterPage + 2;
-  } else if (page == 2) {
-    afterPage  = afterPage + 1;
-  }
-
-  for (var plength = beforePage; plength <= afterPage; plength++) {
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
-      continue;
-    }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
-      plength = plength + 1;
-    }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
-      active = "active";
-    }else{ //else leave empty to the active variable
-      active = "";
-    }
-    liTag += `<li class="page-item ${active}" onclick="createPagination(totalPages, ${plength})"><a class="page-link" href="#">${plength}</a></li>`;
-  }
-
-  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
-    if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
-      liTag += `<li class="page-item"><a class="page-link" href="#">...</a></li>`;
-    }
-    liTag += `<li class="page-item last" onclick="createPagination(totalPages, ${totalPages})"><a class="page-link" href="#">${totalPages}</a></li>`;
-  }
-
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-    liTag += `<li class="page-item" onclick="createPagination(totalPages, ${page + 1})"><a class="page-link" href="#">Next <i class="fas fa-angle-right"></i></a></li>`;
-  }
-  ulPagination.innerHTML = liTag; //add li tag inside ul tag
-  return liTag; //reurn the li tag
+  return ulPagination.innerHTML = createPagination(totalPages, page);
 }
 
 function investorAbout() {
@@ -197,10 +177,10 @@ function newsSwiper() {
       dynamicBullets: true,
       clickable: true,
     },
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-    },
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
+    // },
     mousewheel: true,
     breakpoints: {
       0: {
